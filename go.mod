f5acdc47ef6f114538562adfd08e68f9ca752d56
yosep-alodokter <yosep.nurawan@alodokter.com>
2022-04-19 23:37:44 +0700

    update function with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/yosepnurawan/go-say-hello
+module github.com/yosepnurawan/go-say-hello/v2
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "hello world developer"
+func SayHello(name string) string {
+	return "hello world developer " +name
 }
